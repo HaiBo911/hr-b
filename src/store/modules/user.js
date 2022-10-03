ce62de07c9d7d5300455d7f5bd88c785c48b4579
Haibo_911 <948376725@qq.com>
2022-10-03 23:10:00 +0800

    封装Vuex的登录Action并处理token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,33 @@
-const state = {}
-const mutations = {}
-const actions = {}
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+const state = {
+  token: getToken() // 设置token初始状态   token持久化 => 放到缓存中
+}
+const mutations = {
+  // 设置token
+  setToken(state, token) {
+    state.token = token // 存储在vuex中
+    setToken(token) // 本地存储, 实现数据持久化
+  },
+  // 删除token
+  removeToken(state) {
+    state.token = null // 清除token
+    removeToken() // 清除本地存储token
+  }
+}
+const actions = {
+  // 定义login action  也需要参数 调用action时 传递过来的参数
+  async login(context, data) {
+    const result = await login(data) // 实际上就是一个promise  result就是执行的结果
+    // axios默认给数据加了一层data
+    if (result.data.success) {
+      // 表示登录接口调用成功 也就是意味着你的用户名和密码是正确的
+      // 现在有用户token
+      // actions 修改state 必须通过mutations
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 
 export default {
   namespaced: true,
