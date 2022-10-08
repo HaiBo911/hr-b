61b5d5ac6883d79bd6b1664b2021bd1dfaad0911
Haibo_911 <948376725@qq.com>
2022-10-08 17:01:55 +0800

    完成获取用户资料接口和token注入

--- src/api/user.js
+++ src/api/user.js
@@ -1,11 +1,17 @@
 import request from '@/utils/request'
 
+// 登录接口
 export function login(data) {
   return request({
     url: '/sys/login',
     method: 'post',
     data
   })
+}
+
+// 获取用户的基本资料
+export function getUserInfo() {
+  return request.post('/sys/profile')
 }
 
 export function getInfo(token) { }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
+import store from '../store'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // 基地址
@@ -9,6 +10,11 @@ const service = axios.create({
 // 配置请求拦截器
 service.interceptors.request.use(config => {
   // 请求拦截配置
+
+  // 统一注入token
+  if (store.getters.token) {
+    config.headers['Authorization'] = 'Bearer ${store.getters.token}'
+  }
   return config
 })
 
